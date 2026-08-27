53529f6217f936e592334274064dcb12eb4e6002
AndreKaled <andre.andrade@icomp.ufam.edu.br>
2026-08-26 20:10:14 -0400

    add regra pra identificar a URL com protocolo, dominio, caminho e porta

--- URL.g4
+++ URL.g4
@@ -1,3 +1,12 @@
 lexer grammar URL;
 
-URL: 'http://example.com';
+URL: PROTOCOLO DOMINIO PORTA? CAMINHO?;
+
+PROTOCOLO: 'http://' | 'https://' | 'ftp://';
+
+DOMINIO: [a-zA-Z]+ ('.' [a-zA-Z]+)+;
+
+CAMINHO: ('/' [a-zA-Z0-9._-]+)+;
+
+PORTA: ':' [0-9]+;
+
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import sys
 
-print(sys.argv[1])
+from antlr4 import InputStream, CommonTokenStream
+from URL import URL
+
+if len(sys.argv) != 2:
+    print(f"Uso: python3 main.py <URL>")
+    exit(1)
+
+texto = sys.argv[1]
+
+input_stream = InputStream(texto)
+lexer = URL(input_stream)
+tokens = CommonTokenStream(lexer)
+
+tokens.fill()
+
+for token in tokens.tokens:
+    if token.type != -1: # EOF
+        nome = lexer.symbolicNames[token.type]
+        print(f"TOKEN: {nome}")
+        print(f"VALOR: {token.text}")
